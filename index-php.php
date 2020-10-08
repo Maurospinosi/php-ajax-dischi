eeac2109a15e06fd77a1138d3514d05ee0c952a6
Mauro <mauro.spinosi94@gmail.com>
2020-10-08 13:08:34 +0200

    Inclusione Jquery e handlebars

--- index-php.php
+++ index-php.php
@@ -1,3 +1,5 @@
+<?php include "db.php" ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -13,8 +15,18 @@
     <!-- /header: logo -->
 <!-- ------------------------------------------------------------------------     -->
     <!-- Main : copertine , artisti ed anno dell'album stampato -->
-
+    <main>
+      <div class="container flex">
+        <?php foreach ($database as $album){ ?>
+          <ul>
+            <li><img src="<?php echo $album["poster"] ?>" alt="poster"></li>
+            <li><?php echo $album["title"]; ?></li>
+            <li><?php echo $album["author"] ?></li>
+            <li><?php echo $album["year"] ?></li>
+          </ul>
+        <?php } ?>
+      </div>
+    </main>
     <!-- /Main : copertine , artisti ed anno dell'album stampato -->
-    <script type="text/javascript" src="dist/app.js"></script>
-   </body>
+  </body>
 </html>
---
+++ partials/header.php
@@ -0,0 +1,7 @@
+<header>
+  <div>
+    <span class="img_logo">
+      <img src="img/logo.png" alt="logo">
+    </span>
+  </div>
+</header>
